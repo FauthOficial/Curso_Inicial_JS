223f310533b0be708889c9bc5a5894c19c5dd595
LorenzoFauth <fauthoficial@gmail.com>
2023-10-09 11:45:20 -0300

    Arquivos das aulas com exercicios inclusos
    Add de todos arquivos com aplicações de Javascript incluídos em HTML5 E CSS3

---
+++ Aula12/EX0013.js
@@ -0,0 +1,13 @@
+var agora = new Date()
+var diaSem = 1
+
+console.log(diaSem)
+
+switch(diaSem){
+    case 0:
+        console.log('Domingo')
+        break
+    case 1:
+        console.log('Segunda')
+        break
+}
